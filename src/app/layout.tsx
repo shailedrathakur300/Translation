f0accf6900a977980b68f1cf8a244f91c281d619
shailendrathakur300 <shailendrathakuracademic123@gmail.com>
2024-09-14 13:30:30 +0530

    Setup Bg-AccernityUi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <script src="./node_modules/preline/dist/preline.js"></script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
