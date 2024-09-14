ba09ca2cefca03f87cfa1b360a84767e0a1890a3
shailendrathakur300 <shailendrathakuracademic123@gmail.com>
2024-09-14 20:41:09 +0530

    update in categoryTailwind and SvgDecorations

--- src/components/categoryLinks.tsx
+++ src/components/categoryLinks.tsx
@@ -24,8 +24,13 @@ const CategoryLinks: React.FC = () => {
       {categories.map(({ icon: Icon, label }) => (
         <div
           key={label}
-          className="m-1 py-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg 
-          border border-gray-200 text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none bg-neutral-900 text-white"
+          className="m-1 py-2 px-3 inline-flex 
+          items-center gap-x-2 text-sm font-medium 
+          rounded-lg border border-gray-200 
+          bg-white text-gray-800 shadow-sm hover:bg-gray-50
+           disabled:opacity-50 disabled:pointer-events-none
+            dark:bg-neutral-900 dark:border-neutral-700
+             dark:text-white dark:hover:bg-neutral-800"
         >
           <Icon size={40} />
           <p className="text-2xl">{label}</p>
